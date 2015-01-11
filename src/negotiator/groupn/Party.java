f186a6a36ee5a0380d3453f57700da6b43aad019
Francesco Corsini <fra.gvu@hotmail.it>
2015-01-11 15:04:43 +0100

    modelled the party and the Issue

--- src/negotiator/groupn/Party.java
+++ src/negotiator/groupn/Party.java
@@ -1,12 +1,50 @@
 package negotiator.groupn;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map.Entry;
+
+import negotiator.Bid;
+import negotiator.actions.Action;
+import negotiator.issue.Objective;
+import negotiator.utility.Evaluator;
+import negotiator.utility.UtilitySpace;
 
 public class Party {
 
 	private String name;
-	private int hello;
+	private List<Issue> issues;
 	
-	public Party(String name){
+	public Party(String name,UtilitySpace utility){
 		this.name = name;
+		//create issues
+		createIssues(utility);
+		setInitialWeights();
+	}
+	
+	private void createIssues(UtilitySpace utility){
+		for( Entry<Objective, Evaluator> e : utility.getEvaluators()) {
+			String issueName = e.getKey().getName().toString();
+			Issue iss = new Issue(issueName);
+			issues.add(iss);
+		}
+	}
+	
+	private void setInitialWeights(){
+		int n = issues.size();
+		float averageweight = 1/n;
+		for (int i = 0; i < n; i++){
+			issues.get(i).setValue(averageweight);
+		}
+	}
+	
+	public void updateWithBid(Bid bid,Action action){
+		//TODO update somehow the expected weighs
+	}
+	
+	public Double estimateUtility(Bid b){
+		Double d = null;
+		
+		return d;
 	}
 	
 	
