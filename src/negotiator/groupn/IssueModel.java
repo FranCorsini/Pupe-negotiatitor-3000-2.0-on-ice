1f804ac5ff9be1803c381cd1b7c44f97bfdd137f
Francesco Corsini <fra.gvu@hotmail.it>
2015-01-11 16:33:24 +0100

    issueModel now extends EvaluatorDiscrete

--- src/negotiator/groupn/IssueModel.java
+++ src/negotiator/groupn/IssueModel.java
@@ -3,16 +3,15 @@ package negotiator.groupn;
 import negotiator.issue.ISSUETYPE;
 import negotiator.issue.Issue;
 import negotiator.issue.Value;
+import negotiator.utility.EvaluatorDiscrete;
 
-public class IssueModel extends Issue{
+public class IssueModel extends EvaluatorDiscrete{
 
 	private String name;
 	private float value;
 	
 	public IssueModel(String tempName, int n){
-		super(tempName,n);
 		name = tempName;
-		
 	}
 	
 	public void setValue(float a){
@@ -23,21 +22,4 @@ public class IssueModel extends Issue{
 		return value;
 	}
 
-	@Override
-	public boolean checkInRange(Value value) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public String convertToString() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public ISSUETYPE getType() {
-		// TODO Auto-generated method stub
-		return null;
-	}
 }
